7c323860b76d2e991f3387bff89874f7d1a22ca2
Thunderunruly <hyao2302@uni.sydney.edu.au>
2022-10-13 15:50:27 +0800

    close drawer layout

--- app/src/main/java/comp5216/sydney/edu/au/group11/reciplan/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/group11/reciplan/MainActivity.java
@@ -1,10 +1,15 @@
 package comp5216.sydney.edu.au.group11.reciplan;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,22 +19,23 @@ import comp5216.sydney.edu.au.group11.reciplan.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setActionBar(null);
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
+        setSupportActionBar(binding.appBarMain.toolbar);
+        DrawerLayout drawerLayout = binding.drawerLayout;
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.info_favorite,R.id.info_status,R.id.info_information,R.id.person_log)
+                .setOpenableLayout(drawerLayout)
+                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.app_bar_main).build();
+        NavigationUI.setupWithNavController(binding.appBarMain.navView, navController);
+        drawerLayout.open();
     }
-
 }
